5dbc598f9455ff94d3619c974b013daa2b958f2f
oyvind-ege <o.ege@tutanota.de>
2019-03-12 15:52:10 +0100

    Working draw function. Tested with Xs and Os.

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import pprint
 
 from src.Board import Board
 from src.LogicChecker import LogicChecker
+
+board = Board()
+checker = LogicChecker(board)
+
+board.draw()
--- src/Board.py
+++ src/Board.py
@@ -4,7 +4,7 @@ A game board is a list of lists.
 Each sublist represents a data row, and contains symbols.
     A whitespace represents an empty cell
     An 'X' or 'O' (Oh) means the cell is owned by that player
-    The edges and intersections of the game board itself: | or -- or + 
+    The edges and intersections of the game board itself are represented by: | or -- or + 
 
 The game board looks like this (with coordinates):
 
@@ -49,11 +49,16 @@ class Board:
                 _insert_horizontal_dividing_line(row)
 
     def insert(self, symbol, x, y):
-        """
-        Inserts 'symbol' into self.game_board(y, x)
-        """
+        """Insert 'symbol' into self.game_board(y, x)."""
         self.game_board[y][x] = symbol
     
-    def get_list_of_data(self):
-        """Draws the current board on the screen."""
-        return [row for row in self.game_board]
+    def draw(self):
+        """Print the game board to the terminal."""
+        print("\n")
+        for row in self.game_board:
+            for item in row:
+                if item == ' ' or item == 'X' or item == 'O':
+                    print('', end=' ')
+                print(item, end=" ")
+            print('\n')
+        print("\n")
