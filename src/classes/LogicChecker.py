aff8473b90c73b1272df34cee611f409a6b97033
oyvind-ege <o.ege@tutanota.de>
2019-03-18 15:28:10 +0100

    Minor cleanups.

--- src/classes/LogicChecker.py
+++ src/classes/LogicChecker.py
@@ -5,7 +5,7 @@ class LogicChecker:
     def __init__(self, board):
         self.board = board
         self.turn = 1
-        self.symbol_opposites = {'O': 'X', 'X': 'O'}
+        self.player_opposites = {'O': 'X', 'X': 'O'}
 
     def _check_horizontal(self, symbol, x, y):
         if x == 0:
@@ -32,7 +32,7 @@ class LogicChecker:
             return False
     
     def _check_diagonal(self, symbol, x, y):
-        """Check if adding a next_symbol to either (0,0), (4,0), (2,2), (0,4), (4,4) will lead to a complete diagonal."""
+        """Check if adding a next_player to either (0,0), (4,0), (2,2), (0,4), (4,4) will lead to a complete diagonal."""
 
         top_left = self.board.game_board[0][0]
         top_right = self.board.game_board[0][4]
@@ -71,7 +71,7 @@ class LogicChecker:
             return False
 
     def is_this_move_a_victory(self, symbol, x, y):
-        """Determine whether adding next_symbol to x,y of the game board would lead to victory. Return boolean."""
+        """Determine whether adding next_player to x,y of the game board would lead to victory. Return boolean."""
 
 
         horizontal_win = self._check_horizontal(symbol, x, y)
@@ -82,19 +82,17 @@ class LogicChecker:
             or diagonal_win 
             or vertical_win)
 
-    def would_this_be_a_draw(self, symbol, empty_tiles_coordinates):
+    def would_this_be_a_draw(self, current_player, empty_tiles_coordinates):
         """Checks whether the current gameboard setting results in a draw.
-
-        Accepts a string next_symbol 'X' or 'Y', and a list of tuples. 
-            The string represents the player with the current move.
-            The tuples contain coordinates for all empty tiles.
-        
+                Accepts a string next_player 'X' or 'Y', and a list of tuples. 
+                    The string represents the player with the current move.
+                    The tuples are the coordinates for all empty tiles.
         """
         
-        next_symbol = self.symbol_opposites[symbol] #The code here makes predictions about next round
+        next_player = self.player_opposites[current_player] #The code here makes predictions about next round
 
-        if self.turn >= 7 and self.turn < 8: #There are two open tiles left, next_symbol == 'O'
-    
+        if self.turn == 7: #There are two open tiles left
+
             current_positions_as_dict = self.board.get_tiles_as_dict()
 
             """There are two scenarios. One takes place if player 'O' chooses one tile; the other if he chooses the other tile."""
@@ -107,30 +105,30 @@ class LogicChecker:
             # This means the opposing player must choose empty_tile_coordinates[1]
             # So we need to figure out whether this combination would result in a draw
 
-            scenario1_future_positions[next_symbol].append(empty_tiles_coordinates[0])
+            scenario1_future_positions[next_player].append(empty_tiles_coordinates[0])
 
             scenario1_board = Board(setup=scenario1_future_positions)
             scenario1_logic = LogicChecker(scenario1_board)
 
-            scenario1_boolean = (   not self.is_this_move_a_victory(symbol, *empty_tiles_coordinates[0]) 
-                                and not scenario1_logic.is_this_move_a_victory(self.symbol_opposites[next_symbol], *empty_tiles_coordinates[1]))
+            scenario1_boolean = (   not self.is_this_move_a_victory(current_player, *empty_tiles_coordinates[0]) 
+                                and not scenario1_logic.is_this_move_a_victory(self.player_opposites[next_player], *empty_tiles_coordinates[1]))
  
             #Scenario 2
             # Here the scenario is reversed
 
-            scenario2_future_positions[next_symbol].append(empty_tiles_coordinates[1])
+            scenario2_future_positions[next_player].append(empty_tiles_coordinates[1])
 
             scenario2_board = Board(setup=scenario2_future_positions)
             scenario2_logic = LogicChecker(scenario2_board)
 
-            scenario2_boolean = (   not self.is_this_move_a_victory(symbol, *empty_tiles_coordinates[1])
-                                and not scenario2_logic.is_this_move_a_victory(self.symbol_opposites[next_symbol], *empty_tiles_coordinates[0]))
+            scenario2_boolean = (   not self.is_this_move_a_victory(current_player, *empty_tiles_coordinates[1])
+                                and not scenario2_logic.is_this_move_a_victory(self.player_opposites[next_player], *empty_tiles_coordinates[0]))
 
             return ( scenario1_boolean and scenario2_boolean )
 
         elif self.turn >= 8:
 
-            will_there_be_a_victory = self.is_this_move_a_victory(next_symbol, *empty_tiles_coordinates[0])
+            will_there_be_a_victory = self.is_this_move_a_victory(next_player, *empty_tiles_coordinates[0])
             
             return (not will_there_be_a_victory)
         
